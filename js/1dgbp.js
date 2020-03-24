6351264f48fa284ec82780db1eab5f7210441aba
joe <joe.ortiz@hotmail.co.uk>
2020-03-24 18:08:08 +0000

    Added MAP soln visualisation and sweep message passing schedule

--- js/1dgbp.js
+++ js/1dgbp.js
@@ -47,11 +47,11 @@ function sliceMat(mat,i1,j1,i2,j2) {
 class FactorGraph {
   constructor() {
     this.var_nodes = [];
-    this.smoothness_factors = [];
-    this.meas_factors = [];
+    this.factors = [];
 
     this.sweep_ix = 0;
     this.forward = 1;
+    this.past_first_meas = 0;
   }
 
   update_beliefs() {
@@ -61,12 +61,10 @@ class FactorGraph {
   }
 
   send_messages() {
-    for(var c=0; c<this.smoothness_factors.length; c++) {
-      this.smoothness_factors[c].send_mess();
+    for(var c=0; c<this.factors.length; c++) {
+      this.factors[c].send_both_mess();
     }
-    for(var c=0; c<this.meas_factors.length; c++) {
-      this.meas_factors[c].send_mess();
-    }
+
   }
 
   sync_iter() {
@@ -79,14 +77,10 @@ class FactorGraph {
     var gamma = (x_meas - x_var_lhs) / (x_var_rhs - x_var_lhs);
     const meas_jac = new m.Matrix([[1 - gamma, gamma]]);
 
-    const new_factor = new LinearFactor(meas, meas_jac, 2, adj_var_ids, meas_std, "measurement", x_meas);
-    new_factor.adj_beliefs.push(this.var_nodes[adj_var_ids[0]].belief);
-    new_factor.adj_beliefs.push(this.var_nodes[adj_var_ids[1]].belief);
-    new_factor.messages.push(new Gaussian([[0]], [[0]]));
-    new_factor.messages.push(new Gaussian([[0]], [[0]]));
-    this.meas_factors.push(new_factor);
-    this.var_nodes[adj_var_ids[0]].adj_factors.push(new_factor)
-    this.var_nodes[adj_var_ids[1]].adj_factors.push(new_factor)
+    this.factors[adj_var_ids[0]].jacs.push(meas_jac);
+    this.factors[adj_var_ids[0]].meas.push(meas);
+    this.factors[adj_var_ids[0]].lambdas.push(1 / Math.pow(meas_std, 2));
+    this.factors[adj_var_ids[0]].compute_factor();
   }
 
   computeMAP() {
@@ -98,28 +92,20 @@ class FactorGraph {
     const bigEta = m.Matrix.zeros(tot_dofs, 1);
     const bigLam = m.Matrix.zeros(tot_dofs, tot_dofs);
 
-    for(var c=0; c<this.smoothness_factors.length; c++) {
-      var ix = this.smoothness_factors[c].adj_var_ids[0];
-      bigEta.set(ix, 0, bigEta.get(ix, 0) + this.smoothness_factors[c].factor.eta.get(0, 0));
-      bigEta.set(ix+1, 0, bigEta.get(ix+1, 0) + this.smoothness_factors[c].factor.eta.get(1, 0));
-      bigLam.set(ix, ix, bigLam.get(ix, ix) + this.smoothness_factors[c].factor.lam.get(0, 0));
-      bigLam.set(ix+1, ix, bigLam.get(ix+1, ix) + this.smoothness_factors[c].factor.lam.get(1, 0));
-      bigLam.set(ix, ix+1, bigLam.get(ix, ix+1) + this.smoothness_factors[c].factor.lam.get(0, 1));
-      bigLam.set(ix+1, ix+1, bigLam.get(ix+1, ix+1) + this.smoothness_factors[c].factor.lam.get(1, 1));
+    for(var c=0; c<this.factors.length; c++) {
+      var ix = this.factors[c].adj_var_ids[0];
+      bigEta.set(ix, 0, bigEta.get(ix, 0) + this.factors[c].factor.eta.get(0, 0));
+      bigEta.set(ix+1, 0, bigEta.get(ix+1, 0) + this.factors[c].factor.eta.get(1, 0));
+      bigLam.set(ix, ix, bigLam.get(ix, ix) + this.factors[c].factor.lam.get(0, 0));
+      bigLam.set(ix+1, ix, bigLam.get(ix+1, ix) + this.factors[c].factor.lam.get(1, 0));
+      bigLam.set(ix, ix+1, bigLam.get(ix, ix+1) + this.factors[c].factor.lam.get(0, 1));
+      bigLam.set(ix+1, ix+1, bigLam.get(ix+1, ix+1) + this.factors[c].factor.lam.get(1, 1));
     }
-    for(var c=0; c<this.meas_factors.length; c++) {
-      var ix = this.meas_factors[c].adj_var_ids[0];
-      bigEta.set(ix, 0, bigEta.get(ix, 0) + this.meas_factors[c].factor.eta.get(0, 0));
-      bigEta.set(ix+1, 0, bigEta.get(ix+1, 0) + this.meas_factors[c].factor.eta.get(1, 0));
-      bigLam.set(ix, ix, bigLam.get(ix, ix) + this.meas_factors[c].factor.lam.get(0, 0));
-      bigLam.set(ix+1, ix, bigLam.get(ix+1, ix) + this.meas_factors[c].factor.lam.get(1, 0));
-      bigLam.set(ix, ix+1, bigLam.get(ix, ix+1) + this.meas_factors[c].factor.lam.get(0, 1));
-      bigLam.set(ix+1, ix+1, bigLam.get(ix+1, ix+1) + this.meas_factors[c].factor.lam.get(1, 1));
-    }
+
 
     const bigCov = m.inverse(bigLam);
     const means = bigCov.mmul(bigEta);
-    return means;
+    return [means, bigCov];
   }
 
   compare_to_MAP() {
@@ -129,7 +115,7 @@ class FactorGraph {
     }
 
     const means = new m.Matrix([gbp_means]);
-    const map = this.computeMAP();
+    const map = this.computeMAP()[0];
     var av_diff = (map.sub(means.transpose())).norm();
     return av_diff;
   }
@@ -137,28 +123,33 @@ class FactorGraph {
   sweep_step() {
     // Prepare for next step
     var next_ix = 0;
+    var next_forward = this.forward;
     if (this.forward) {
-      if (this.sweep_ix == this.var_nodes.length -1) {
-        next_ix = this.sweep_ix - 1;
-      } else {
-        next_ix = this.sweep_ix + 1;
-      }
+      next_ix = this.sweep_ix + 1;
+      if (this.sweep_ix == this.var_nodes.length - 2) {
+        next_forward = 0;
+      } 
     } else {
-      if (this.sweep_ix == 0) {
-        next_ix = this.sweep_ix + 1;
-      } else {
-        next_ix = this.sweep_ix - 1;
+      next_ix = this.sweep_ix - 1;
+      if (this.sweep_ix == 1) {
+        next_forward = 1;
       }
     }
 
-    this.var_nodes[this.sweep_ix].update_belief();
-    // Connected factors which also link to the var node on the right send messages
-    for (var i=0; i<this.var_nodes[this.sweep_ix].adj_factors.length; i++) {
-      if (this.var_nodes[this.sweep_ix].adj_factors[i].adj_var_ids[1] == next_ix) {
-        this.var_nodes[this.sweep_ix].adj_factors[i].send_mess();
+    if (this.forward) {
+      if (this.factors[this.sweep_ix].meas.length > 1) {
+        this.past_first_meas = 1;
       }
+      this.factors[this.sweep_ix].send_mess(this.forward);
+    } else {
+      this.factors[next_ix].send_mess(this.forward);
     }
+    if (this.past_first_meas) {
+      this.var_nodes[next_ix].update_belief();
+    }
+
     this.sweep_ix = next_ix;
+    this.forward = next_forward;
   }
 }
 
@@ -166,7 +157,7 @@ class VariableNode {
   constructor(dofs, var_id) {
     this.dofs = dofs;
     this.var_id = var_id;
-    this.belief = new Gaussian(m.Matrix.zeros(dofs, 1), m.Matrix.eye(dofs, dofs).mul(1e-7));
+    this.belief = new Gaussian(m.Matrix.zeros(dofs, 1), m.Matrix.zeros(dofs, dofs));
 
     this.adj_factors = [];
   }
@@ -191,42 +182,55 @@ class VariableNode {
 
 
 class LinearFactor {
-  constructor(meas, jac, dofs, adj_var_ids, meas_model_std, type, x_meas) {
-    this.type = type;
-    this.x_meas = x_meas;
-    this.jac = jac;
+  constructor(dofs, adj_var_ids) {
+    this.dofs = dofs;
     this.adj_var_ids = adj_var_ids;
     this.adj_beliefs = [];
+
+    // To compute factor when factor is combination of many factor types (e.g. measurement and smoothness)
+    this.jacs = [];
+    this.meas = [];
+    this.lambdas = [];
+    this.factor = new Gaussian(m.Matrix.zeros(dofs, 1), m.Matrix.zeros(dofs, dofs));
+
     this.messages = [];
+  }
 
-    this.meas = meas;
-    var lambda = 1 / Math.pow(meas_model_std, 2);
-    this.factor = new Gaussian(jac.transpose().mul(lambda * this.meas), 
-                                     jac.transpose().mmul(jac).mul(lambda));
-
+  compute_factor() {
+    this.factor.eta = m.Matrix.zeros(this.dofs, 1);
+    this.factor.lam = m.Matrix.zeros(this.dofs, this.dofs);
+    for (var i=0; i<this.jacs.length; i++) {
+      this.factor.eta.add(this.jacs[i].transpose().mul(this.lambdas[i] * this.meas[i]));
+      this.factor.lam.add(this.jacs[i].transpose().mmul(this.jacs[i]).mul(this.lambdas[i]));
+    }
   }
 
   // Only for bipartite factors where the adjacent vars have 1 dof
-  send_mess() {
-    const mess0 = new Gaussian([[0]], [[0]]);
-    const mess1 = new Gaussian([[0]], [[0]]);
+  send_mess(ix) {
+    if (ix) {
+      const mess1 = new Gaussian([[0]], [[0]]);
+      mess1.eta = new m.Matrix([[this.factor.eta.get(1, 0) - 
+          this.factor.lam.get(1, 0) * (this.factor.eta.get(0, 0) + this.adj_beliefs[0].eta.get(0, 0) - this.messages[0].eta.get(0, 0)) / 
+          (this.factor.lam.get(0, 0) + this.adj_beliefs[0].lam.get(0, 0) - this.messages[0].lam.get(0, 0))]]);
+      mess1.lam = new m.Matrix([[this.factor.lam.get(1, 1) - 
+          this.factor.lam.get(1, 0) * this.factor.lam.get(0, 1) / 
+          (this.factor.lam.get(0, 0) + this.adj_beliefs[0].lam.get(0, 0) - this.messages[0].lam.get(0, 0))]]);
+      this.messages[1] = mess1;
+    } else {
+      const mess0 = new Gaussian([[0]], [[0]]);
+      mess0.eta = new m.Matrix([[this.factor.eta.get(0, 0) - 
+          this.factor.lam.get(0, 1) * (this.factor.eta.get(1, 0) + this.adj_beliefs[1].eta.get(0, 0) - this.messages[1].eta.get(0, 0)) / 
+          (this.factor.lam.get(1, 1) + this.adj_beliefs[1].lam.get(0, 0) - this.messages[1].lam.get(0, 0))]]);
+      mess0.lam = new m.Matrix([[this.factor.lam.get(0, 0) - 
+          this.factor.lam.get(0, 1) * this.factor.lam.get(1, 0) / 
+          (this.factor.lam.get(1, 1) + this.adj_beliefs[1].lam.get(0, 0) - this.messages[1].lam.get(0, 0))]]);
+      this.messages[0] = mess0;
+    }
+  }
 
-    mess0.eta = new m.Matrix([[this.factor.eta.get(0, 0) - 
-        this.factor.lam.get(0, 1) * (this.factor.eta.get(1, 0) + this.adj_beliefs[1].eta.get(0, 0) - this.messages[1].eta.get(0, 0)) / 
-        (this.factor.lam.get(1, 1) + this.adj_beliefs[1].lam.get(0, 0) - this.messages[1].lam.get(0, 0))]]);
-    mess0.lam = new m.Matrix([[this.factor.lam.get(0, 0) - 
-        this.factor.lam.get(0, 1) * this.factor.lam.get(1, 0) / 
-        (this.factor.lam.get(1, 1) + this.adj_beliefs[1].lam.get(0, 0) - this.messages[1].lam.get(0, 0))]]);
-
-    mess1.eta = new m.Matrix([[this.factor.eta.get(1, 0) - 
-        this.factor.lam.get(1, 0) * (this.factor.eta.get(0, 0) + this.adj_beliefs[0].eta.get(0, 0) - this.messages[0].eta.get(0, 0)) / 
-        (this.factor.lam.get(0, 0) + this.adj_beliefs[0].lam.get(0, 0) - this.messages[0].lam.get(0, 0))]]);
-    mess1.lam = new m.Matrix([[this.factor.lam.get(1, 1) - 
-        this.factor.lam.get(1, 0) * this.factor.lam.get(0, 1) / 
-        (this.factor.lam.get(0, 0) + this.adj_beliefs[0].lam.get(0, 0) - this.messages[0].lam.get(0, 0))]]);
-
-    this.messages[0] = mess0;
-    this.messages[1] = mess1;
+  send_both_mess(){
+    this.send_mess(0);
+    this.send_mess(1);
   }
 }
 
@@ -244,12 +248,17 @@ function create1Dgraph(n_var_nodes, smoothness_std) {
   // Create smoothness factors
   const smoothness_jac = new m.Matrix([[-1, 1]]);
   for(var i=0; i<(n_var_nodes-1); i++) {
-    const new_factor = new LinearFactor(0., smoothness_jac, 2, [i, i+1], smoothness_std, "smoothness" , "");
+    const new_factor = new LinearFactor(2, [i, i+1], );
+    new_factor.jacs.push(smoothness_jac);
+    new_factor.meas.push(0.);
+    new_factor.lambdas.push(1 / Math.pow(smoothness_std, 2));
+
     new_factor.adj_beliefs.push(graph.var_nodes[i].belief);
     new_factor.adj_beliefs.push(graph.var_nodes[i+1].belief);
     new_factor.messages.push(new Gaussian([[0]], [[0]]));
     new_factor.messages.push(new Gaussian([[0]], [[0]]));
-    graph.smoothness_factors.push(new_factor);
+    new_factor.compute_factor();
+    graph.factors.push(new_factor);
     graph.var_nodes[i].adj_factors.push(new_factor);
     graph.var_nodes[i+1].adj_factors.push(new_factor);
   }
@@ -277,21 +286,28 @@ var meas_model_std = 50;
 var smoothness_std = 50;
 
 const graph = create1Dgraph(10, 50);
-// Set initial position of nodes
-for(var c=0; c<graph.var_nodes.length; c++) {
-  graph.var_nodes[c].belief.eta = new m.Matrix([[0.05]]);
-  graph.var_nodes[c].belief.lam = new m.Matrix([[1e-4]]);
-}
+
+measurements = []
 
 var GBP_on = 0;
 var GBP_sweep = 0;
 var n_iters = 0;
 var dist = 0; // Average distance of belief means from MAP solution
 var iters_per_sec = 25;
+var disp_MAP = 0;
 
 function syncGBP() {
+  if (n_iters == 0) {
+    // Set initial position of nodes
+    for(var c=0; c<graph.var_nodes.length; c++) {
+      graph.var_nodes[c].belief.eta = new m.Matrix([[0.05]]);
+      graph.var_nodes[c].belief.lam = new m.Matrix([[1e-4]]);
+    }
+  }
   graph.sync_iter();
-  dist = graph.compare_to_MAP();
+  if (!(n_iters == 0)) {
+    dist = graph.compare_to_MAP();   
+  }
   n_iters++;
 }
 
@@ -303,8 +319,13 @@ function drawCanvasBackground() {
 function drawNodes() {
   for(var c=0; c<graph.var_nodes.length; c++) {
     var x = nodes_x_offset + c*node_x_spacing;
-    var y = graph.var_nodes[c].belief.getMean().get(0, 0);
-    var var_y = graph.var_nodes[c].belief.getCov().get(0, 0);
+    if (graph.var_nodes[c].belief.lam.get(0, 0) == 0) {
+      var y = 500;
+      var var_y = Math.pow(50, 2);
+    } else {
+      var y = graph.var_nodes[c].belief.getMean().get(0, 0);
+      var var_y = graph.var_nodes[c].belief.getCov().get(0, 0);
+    }
 
     // Draw means
     ctx.beginPath();
@@ -318,29 +339,50 @@ function drawNodes() {
     ctx.lineTo(x, parseInt(y) - parseInt(Math.sqrt(var_y)));
     ctx.strokeStyle = "#0095DD";
     ctx.stroke();
-
   }
 }
 
-function drawMeasurements() {
-  for(var c=0; c<graph.meas_factors.length; c++) {
-    x_meas = graph.meas_factors[c].x_meas;
-    y = graph.meas_factors[c].meas;
+function drawMeasurements(meas_model_std) {
+  for(var c=0; c<measurements.length; c++) {
+    x = measurements[c].x;
+    y = measurements[c].y;
 
     // Draw means
     ctx.beginPath();
-    ctx.arc(x_meas, y, node_radius, 0, Math.PI*2);
-    // ctx.rect(x_meas - sidelength/2, y - sidelength/2, sidelength, sidelength);
+    ctx.arc(x, y, node_radius, 0, Math.PI*2);
+    // ctx.rect(x - sidelength/2, y - sidelength/2, sidelength, sidelength);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
     // Draw variances
     ctx.beginPath();
-    ctx.moveTo(x_meas, parseInt(y) - parseInt(meas_model_std));
-    ctx.lineTo(x_meas, parseInt(y) + parseInt(meas_model_std));
+    ctx.moveTo(x, parseInt(y) - parseInt(meas_model_std));
+    ctx.lineTo(x, parseInt(y) + parseInt(meas_model_std));
     ctx.strokeStyle = "red";
     ctx.stroke();
+  }
+}
 
+function drawMAP() {
+  var values = graph.computeMAP();
+  const means = values[0];
+  const bigSigma = values[1];
+  for(var c=0; c<graph.var_nodes.length; c++) {
+    var x = nodes_x_offset + c*node_x_spacing;
+    var y = means.get(c, 0);
+    var var_y = bigSigma.get(c, c);
+
+    // Draw means
+    ctx.beginPath();
+    ctx.arc(x, y, node_radius, 0, Math.PI*2);
+    ctx.strokeStyle = 'green';
+    ctx.stroke();
+    // Draw variances
+    ctx.beginPath();
+    ctx.moveTo(x, parseInt(y) + parseInt(Math.sqrt(var_y)));
+    ctx.lineTo(x, parseInt(y) - parseInt(Math.sqrt(var_y)));
+    ctx.strokeStyle = 'green';
+    ctx.stroke();
   }
 }
 
@@ -365,27 +407,35 @@ function startVis(fps) {
 
 function updateVis() {
   requestAnimationFrame(updateVis);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCanvasBackground();
 
   fpsInterval = 1000 / iters_per_sec;
   now = Date.now();
   elapsed = now - then;
   if (elapsed > fpsInterval) {
     then = now - (elapsed % fpsInterval);
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCanvasBackground();
     if (GBP_on) {
       syncGBP();
-      drawNumIters();
-      drawDistance();
     } else if (GBP_sweep) {
       graph.sweep_step();
-      dist = graph.compare_to_MAP();
-      drawDistance();
+      // dist = graph.compare_to_MAP();
+      // drawDistance();
+      if (graph.forward == 1 && graph.sweep_ix == 0) {
+        // Back and forth sweep complete
+        GBP_sweep = 0;
+      }
     }
-    drawNodes();
-    drawMeasurements();
+  }
+  if (GBP_on) {
+    drawNumIters();
     drawDistance();
+  } 
+  drawNodes();
+  drawMeasurements(meas_model_std);
+  drawDistance();
+  if (disp_MAP) {
+    drawMAP();
   }
 }
 
@@ -397,30 +447,32 @@ function addMeasurement(e) {
     var x_lhs = nodes_x_offset + Math.floor(ix)*node_x_spacing;
     var x_rhs = nodes_x_offset + Math.ceil(ix)*node_x_spacing;
     graph.addLinearMeasurement(relativeY, relativeX, [Math.floor(ix), Math.ceil(ix)], x_lhs, x_rhs, meas_model_std);
+    measurements.push({x: relativeX, y: relativeY})
   }
 }
+
 
 
 // Sliders
 function update_meas_model_std(val) {
   meas_model_std = val;
-  // document.getElementById("mmstd").innerHTML = meas_model_std;
   var lambda = 1 / Math.pow(meas_model_std, 2);
-
-  for(var c=0; c<graph.meas_factors.length; c++) {
-    graph.meas_factors[c].factor.eta = graph.meas_factors[c].jac.transpose().mul(lambda * graph.meas_factors[c].meas)
-    graph.meas_factors[c].factor.lam = graph.meas_factors[c].jac.transpose().mmul(graph.meas_factors[c].jac).mul(lambda);
+  // document.getElementById("mmstd").innerHTML = meas_model_std;
+  for (var c=0; c<graph.factors.length; c++) {
+    for (var i=1; i<graph.factors[c].lambdas.length; i++) {
+      graph.factors[c].lambdas[i] = lambda;
+    }
+    graph.factors[c].compute_factor();
   }
 }
 
 function update_smoothness_std(val) {
   smoothness_std = val;
-  // document.getElementById("sstd").innerHTML = smoothness_std; 
   var lambda = 1 / Math.pow(smoothness_std, 2);
-
-  for(var c=0; c<graph.smoothness_factors.length; c++) {
-    graph.smoothness_factors[c].factor.eta = graph.smoothness_factors[c].jac.transpose().mul(lambda * graph.smoothness_factors[c].meas)
-    graph.smoothness_factors[c].factor.lam = graph.smoothness_factors[c].jac.transpose().mmul(graph.smoothness_factors[c].jac).mul(lambda);
+  // document.getElementById("sstd").innerHTML = smoothness_std; 
+  for (var c=0; c<graph.factors.length; c++) {
+    graph.factors[c].lambdas[0] = lambda;
+    graph.factors[c].compute_factor();
   }
 }
 
@@ -433,22 +485,22 @@ var mm_slider = document.getElementById("meas_model_std");
 mm_slider.oninput = function() { update_meas_model_std(this.value)}
 var s_slider = document.getElementById("smoothness_std");
 s_slider.oninput = function() { update_smoothness_std(this.value)}
-var s_slider = document.getElementById("iters_per_sec");
-s_slider.oninput = function() { update_iters_per_sec(this.value)}
+var i_slider = document.getElementById("iters_per_sec");
+i_slider.oninput = function() { update_iters_per_sec(this.value)}
 
 
 // Buttons
 function start_sweepGBP() {
-  alert("To do")
-  // if (graph.meas_factors.length == 0) {
-  //   alert("You must add a measurement before beginning GBP")
-  // } else {
-  //   GBP_sweep = 1;
-  //   iters_per_sec = 2;
-  // }
+  if (measurements.length == 0) {
+    alert("You must add a measurement before beginning GBP")
+  } else {
+    GBP_sweep = 1;
+    iters_per_sec = 2;
+    GBP_on = 0;
+  }
 }
 function start_syncGBP() {
-  if (graph.meas_factors.length == 0) {
+  if (measurements.length == 0) {
     alert("You must add a measurement before beginning GBP")
   } else {
     if (GBP_on) {
@@ -456,8 +508,19 @@ function start_syncGBP() {
       this.value = ("Resume synchronous GBP");
     } else {
       GBP_on = 1;
+      GBP_sweep = 0;
+      iters_per_sec = i_slider.value;
       this.value = ("Pause synchronous GBP");
     }
+  }
+}
+function display_MAP() {
+  if (disp_MAP == 0) {
+    disp_MAP = 1;
+    this.value = ("Hide MAP");
+  } else {
+    disp_MAP = 0;
+    this.value = ("Display MAP");
   }
 }
 
@@ -471,6 +534,7 @@ function addButton(name, text, func) {
 
 addButton('sync', 'Start synchronous GBP', start_syncGBP);
 addButton('sweep', 'Start a sweep of GBP', start_sweepGBP);
+addButton('map', 'Display MAP', display_MAP);
 
 // On click on canvas
 document.addEventListener("click", addMeasurement, false);
